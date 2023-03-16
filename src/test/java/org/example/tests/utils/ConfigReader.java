5f2e274e7283f29cbabcb8edaa45cc9608dbe21a
Sandeep Kumar <114305424+sandeepk-ralh@users.noreply.github.com>
2023-03-16 09:19:07 -0400

    Removing unnecessary checks.

--- src/test/java/org/example/tests/utils/ConfigReader.java
+++ src/test/java/org/example/tests/utils/ConfigReader.java
@@ -17,91 +17,43 @@ public class ConfigReader {
                 properties.load(reader);
                 reader.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println("Loading config file failed." + e.getMessage());
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            throw new RuntimeException("configuration.properties not found at " + propertyFilePath);
+            System.out.println("configuration.properties file not found at " + propertyFilePath);
         }
     }
 
     public static String getBaseUrl() {
-        String baseUrl = properties.getProperty("baseUrl");
-        if(baseUrl != null) {
-            return baseUrl;
-        } else {
-            System.out.println("baseUrl not specified in the configuration.properties file.");
-            return null;
-        }
+        return properties.getProperty("baseUrl");
     }
 
     public static String getEmailAlphaCCO() {
-        String emailAlphaCCO = properties.getProperty("emailAlphaCCO");
-        if(emailAlphaCCO != null) {
-            return emailAlphaCCO;
-        } else {
-            System.out.println("emailAlphaCCO not specified in the configuration.properties file.");
-            return null;
-        }
+        return properties.getProperty("emailAlphaCCO");
     }
 
     public static String getPassword() {
-        String password = properties.getProperty("password");
-        if(password != null) {
-            return password;
-        } else {
-            System.out.println("password not specified in the configuration.properties file.");
-            return null;
-        }
+        return properties.getProperty("password");
     }
 
     public static String getInvalidUsername() {
-        String invalidEmail = properties.getProperty("invalidEmail");
-        if(invalidEmail != null) {
-            return invalidEmail;
-        } else {
-            System.out.println("invalidEmail not specified in the configuration.properties file.");
-            return null;
-        }
+        return properties.getProperty("invalidEmail");
     }
 
     public static String getInvalidPassword() {
-        String invalidPassword = properties.getProperty("invalidPassword");
-        if(invalidPassword != null) {
-            return invalidPassword;
-        } else {
-            System.out.println("invalidPassword not specified in the configuration.properties file.");
-            return null;
-        }
+        return properties.getProperty("invalidPassword");
     }
 
     public static String getNewPassword() {
-        String newPassword = properties.getProperty("password");
-        if(newPassword != null) {
-            return newPassword;
-        } else {
-            System.out.println("newPassword not specified in the configuration.properties file.");
-            return null;
-        }
+        return properties.getProperty("password");
     }
 
     public static String getOutlookPassword() {
-        String outlookPassword = properties.getProperty("outlookPassword");
-        if(outlookPassword != null) {
-            return outlookPassword;
-        } else {
-            System.out.println("outlookPassword not specified in the configuration.properties file.");
-            return null;
-        }
+        return properties.getProperty("outlookPassword");
     }
 
     public static String getOutlookUrl() {
-        String outlookUrl = properties.getProperty("outlookUrl");
-        if(outlookUrl != null) {
-            return outlookUrl;
-        } else {
-            System.out.println("outlookUrl not specified in the configuration.properties file.");
-            return null;
-        }
+        return properties.getProperty("outlookUrl");
     }
 }
